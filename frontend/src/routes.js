de7b1a0fad4165e99a38c006f9bd72cfbf68837d
Mathias Klippinge <mathias.klippinge@gmail.com>
2017-02-28 16:25:33 +0100

    Restructure routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -6,9 +6,9 @@ import store from 'store'
 import Layout from 'views/layout'
 import EventsSearch from 'views/events/search'
 import EventDetails from 'views/events/details'
-import Dashboard from 'views/dashboard'
 import FailuresSearch from 'views/failures/search'
 import FailureDetails from 'views/failures/details'
+import Dashboard from 'views/dashboard'
 
 export const history = syncHistoryWithStore(browserHistory, store)
 
@@ -16,9 +16,9 @@ export default (
   <Router history={history}>
     <Route path='/' component={Layout}>
       <IndexRedirect to='/events' />
+      <Route path='/dashboard' component={Dashboard} />
       <Route path='/events' component={EventsSearch} />
       <Route path='/events/:id' component={EventDetails} />
-      <Route path='/dashboard' component={Dashboard} />
       <Route path='/failures' component={FailuresSearch} />
       <Route path='/failures/:id' component={FailureDetails} />
     </Route>
