fee84c142cbd7e575c8bfc05e81f6925204424bc
Mathias Klippinge <mathias.klippinge@gmail.com>
2017-03-12 20:47:26 +0100

    Rudimentary dashboard

--- frontend/src/views/dashboard/index.js
+++ frontend/src/views/dashboard/index.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 
+import Badge from 'components/badge'
+import { fetchFailureCount } from 'actions/dashboard/failure-count'
+
 export class Dashboard extends Component {
+  static get propTypes () {
+    return {
+      fetchFailureCount: PropTypes.func.isRequired,
+
+      xhrStatus: PropTypes.shape({
+        isFetchingFailureCount: PropTypes.bool
+      })
+    }
+  }
+
+  componentDidMount () {
+    !this.props.failureCount &&
+      this.props.fetchFailureCount()
+  }
+
   render () {
     return (
-      <div className='dashboard'>Dashy board</div>
+      <div className='dashboard'>
+        {
+          <Badge
+            classCondition={this.props.dashboard.failureCount === 0}
+            text={this.props.dashboard.failureCount}
+            loading={this.props.xhrStatus.isFetchingFailureCount} />
+        }
+      </div>
     )
   }
 }
 
 export default connect(
-  (state) => state, {}
+  (state) => state, {
+    fetchFailureCount
+  }
 )(Dashboard)
