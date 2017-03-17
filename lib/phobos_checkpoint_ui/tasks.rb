f0e9333bcc198c090b2ab24262bef1a057db5238
Mathias Klippinge <mathias.klippinge@gmail.com>
2017-03-17 10:37:37 +0100

    Change slightly how copy_assets task work so it doesnt wipe the public folder entirely

--- lib/phobos_checkpoint_ui/tasks.rb
+++ lib/phobos_checkpoint_ui/tasks.rb
@@ -8,12 +8,13 @@ module PhobosCheckpointUI
     namespace :phobos_checkpoint_ui do
       desc 'Copy Assets to ./public folder. It creates the folder if it doesn\'t exist'
       task :copy_assets do
-        folder = File.join(Dir.pwd, 'public')
-        assets = File.expand_path(File.join(File.dirname(__FILE__), '../../assets'))
-        FileUtils.rm_rf(folder)
-        FileUtils.mkdir_p(folder)
-        FileUtils.cp_r(assets, File.join(folder, 'assets'))
-        FileUtils.mv(File.join(folder, 'assets/index.html'), File.join(folder, 'index.html'))
+        public = File.join(Dir.pwd, 'public')
+        assets = File.join(Dir.pwd, 'public', 'assets')
+        content = File.expand_path(File.join(File.dirname(__FILE__), '../../assets'))
+        FileUtils.rm_rf(assets)
+        FileUtils.mkdir_p(assets)
+        FileUtils.cp_r(content, public)
+        FileUtils.mv(File.join(assets, 'index.html'), File.join(public, 'index.html'))
       end
     end
   end
--- lib/phobos_checkpoint_ui/version.rb
+++ lib/phobos_checkpoint_ui/version.rb
@@ -1,3 +1,3 @@
 module PhobosCheckpointUI
-  VERSION = '1.3.1'
+  VERSION = '1.3.2'
 end
