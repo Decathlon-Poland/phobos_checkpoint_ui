b8756ae54bb4de4afeda307bec2c1fab30015af4
Mathias Klippinge <mathias.klippinge@gmail.com>
2018-06-29 14:47:23 +0200

    Make it possible to configure middleware logger options

--- lib/phobos_checkpoint_ui/app.rb
+++ lib/phobos_checkpoint_ui/app.rb
@@ -1,8 +1,22 @@
 module PhobosCheckpointUI
   module App
-    def self.new(api_app:, config: {}, saml_handler: PhobosCheckpointUI::SamlHandler, logger_middleware: Rack::NullLogger)
-      StaticApp.use(logger_middleware)
+    def self.new(api_app:,
+                 config: {},
+                 saml_handler: PhobosCheckpointUI::SamlHandler,
+                 logger_middleware: nil
+                )
+
       StaticApp.configs = config
+
+      if logger_middleware.present?
+        middleware_klass = logger_middleware.dig(:class)
+        raise ':class key missing in :logger_middleware parameter' unless middleware_klass
+
+        StaticApp.use(middleware_klass, logger_middleware.dig(:opts) || {})
+      else
+        StaticApp.use(Rack::NullLogger)
+      end
+
       Rack::URLMap.new(
         '/' => StaticApp.new(api_app, saml_handler),
         '/ping' => Proc.new { |env| ['200', { 'Content-Type' => 'text/plain' }, ['PONG']] }
